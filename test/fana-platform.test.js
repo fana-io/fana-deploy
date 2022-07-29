c48892669aad5781d55634fa223d774ec0fb68cb
yoorhim <choi.yoorhim@gmail.com>
2022-07-29 11:10:44 -0400

    Initial commit

---
+++ test/fana-platform.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const FanaPlatform = require('../lib/fana-platform-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fana-platform-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new FanaPlatform.FanaPlatformStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
